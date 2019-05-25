f77517cd9526ba466ee075f2b4b508791cfaeb6e
120356aa <yurlovandrew@gmail.com>
2019-05-25 15:58:20 -0700

    skills page heading

--- src/components/skills/skillspage.js
+++ src/components/skills/skillspage.js
@@ -11,7 +11,7 @@ const Skills = () => {
     <Wrap>
       <Header />
       <InnerBox>
-        
+        <div className="heading">My Skills</div>
       </InnerBox>
     </Wrap>
   );
--- src/components/skills/skillspageStyles.js
+++ src/components/skills/skillspageStyles.js
@@ -8,9 +8,20 @@ export const Wrap = styled.div`
   background-color: #fff;
   flex-flow: column nowrap;
   align-items: center;
+  z-index: 1;
 `;
 
   export const InnerBox = styled.div`
-    border: 1px solid red;
+    // border: 1px solid red;
     width: 100%;
+
+    .heading {
+      font-family: 'Source Sans Pro', sans-serif;
+      font-size: 24px;
+      font-weight: 600;
+      color: #949494;
+      text-align: center;
+      padding: 15px 0 15px 0;
+      box-shadow: 0 0 8px #D2D2D2;
+    }
   `;
